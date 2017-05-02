8b9dbd85b0739c32cb99ca3e4d5a9f655bcd61ab
markcyr <markcyr@msn.com>
2017-05-01 20:20:53 -0400

    add query 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,36 @@
+$(document).ready(function() {
 
 
+
+//Select the element with id button
+
+$('button').on('click', function () {
+     $( "body" ).toggleClass( "dark" );
+
+  if($(this).hasClass("switch off")) {
+    console.log('hey')
+           $('button').removeClass("switch off").addClass("switch on");
+           $(".status").text("Hi");
+
+
+  } else {
+    $(this).hasClass("switch on");
+     $('button').removeClass("switch on").addClass("switch off");
+      console.log('ho')
+         $(".status").text("Ho");
+
+
+
+  }
+
+
+
+});
+
+
+
+
+
+
+
+});
